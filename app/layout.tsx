0d6d4cc54b1dc2c8d08505dbe2c07aa7e4a48134
Vladimir Leveryev <volodya.leveryev@gmail.com>
2025-06-14 10:38:36 +0900

    Add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import './globals.css';
 
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Sonor',
+}
+
 export default function RootLayout({
   children,
 }: {
