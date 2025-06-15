d6630504725b89748c4d881d0ef6875849da8e47
Vladimir Leveryev <volodya.leveryev@gmail.com>
2025-06-15 22:15:50 +0900

    Add next escaper step

--- app/components/game.tsx
+++ app/components/game.tsx
@@ -5,23 +5,21 @@ import { JSX, useEffect, useRef, useState } from 'react';
 const FIELD_HEIGHT = 30;
 const FIELD_WIDTH = 42;
 
+type Point = { x: number; y: number };
+
 // Догоняющий
-const Chaser = ({ x, y }: { x: number; y: number }) => (
-  <circle cx={x} cy={y} r="2" stroke="Black" strokeWidth=".05" fill="Blue" />
-);
+const Chaser = ({ x, y }: Point) => <circle cx={x} cy={y} r="2" stroke="Black" strokeWidth=".05" fill="Blue" />;
 
 // Новая фишка для догоняющего
-const NewChaser = ({ x, y }: { x: number; y: number }) => (
+const NewChaser = ({ x, y }: Point) => (
   <circle cx={x} cy={y} r="2" stroke="Black" strokeWidth=".05" fill="LightBlue" strokeDasharray={0.1} />
 );
 
 // Убегающий
-const Escaper = ({ x, y }: { x: number; y: number }) => (
-  <circle cx={x} cy={y} r="1" stroke="Black" strokeWidth=".05" fill="Green" />
-);
+const Escaper = ({ x, y }: Point) => <circle cx={x} cy={y} r="1" stroke="Black" strokeWidth=".05" fill="Green" />;
 
 // Новая фишка для убегающего
-const NewEscaper = ({ x, y }: { x: number; y: number }) => (
+const NewEscaper = ({ x, y }: Point) => (
   <circle cx={x} cy={y} r="1" stroke="Black" strokeWidth=".05" fill="LightGreen" strokeDasharray={0.1} />
 );
 
@@ -37,6 +35,20 @@ export default function Game({ props = null }) {
     } else {
       setTurn('escaper');
     }
+  };
+
+  const getNearestPoint = (m: Point, chips: Point[]): Point => {
+    let minLength = Infinity;
+    let chip: Point;
+    for (const c of chips) {
+      const v1 = { x: m.x - c.x, y: m.y - c.y };
+      const length = Math.sqrt(v1.x * v1.x + v1.y * v1.y);
+      if (minLength > length) {
+        minLength = length;
+        chip = { x: c.x, y: c.y };
+      }
+    }
+    return chip;
   };
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -55,15 +67,26 @@ export default function Game({ props = null }) {
           x: ((e.clientX - rect.x) / rect.width) * FIELD_WIDTH,
           y: ((e.clientY - rect.y) / rect.height) * FIELD_HEIGHT,
         };
-        let point2: { x: number; y: number };
+        let point2: Point;
         if (turn === 'escaper') {
-          point2 = { x: mouse.x, y: mouse.y };
+          // Убегающие
+          const point1 = getNearestPoint(mouse, [
+            { x: 4, y: 5 },
+            { x: 4, y: 10 },
+            { x: 4, y: 15 },
+            { x: 4, y: 20 },
+            { x: 4, y: 25 },
+          ]);
+          const vector1 = { x: mouse.x - point1.x, y: mouse.y - point1.y };
+          const length = Math.sqrt(vector1.x * vector1.x + vector1.y * vector1.y);
+          const vector2 = { x: (vector1.x / length) * 2, y: (vector1.y / length) * 2 };
+          point2 = { x: point1.x + vector2.x, y: point1.y + vector2.y };
         } else {
           // Догоняющий
           const point1 = { x: 33, y: 15 };
           const vector1 = { x: mouse.x - point1.x, y: mouse.y - point1.y };
-          const length1 = Math.sqrt(vector1.x * vector1.x + vector1.y * vector1.y);
-          const vector2 = { x: (vector1.x / length1) * 4, y: (vector1.y / length1) * 4 };
+          const length = Math.sqrt(vector1.x * vector1.x + vector1.y * vector1.y);
+          const vector2 = { x: (vector1.x / length) * 4, y: (vector1.y / length) * 4 };
           point2 = { x: point1.x + vector2.x, y: point1.y + vector2.y };
         }
         setPosition(point2);
